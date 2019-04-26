f02723a0534176f4d1336a61ddf99c081dda7700
fylovezx <490122386@qq.com>
2019-04-27 00:27:42 +0800

    runoob 初步布局

---
+++ runoob/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container" style="width:1500px">
+        
+        <div id="header" style="background-color:#9ab488;">
+            <h1 style="margin-bottom:0;">网页头部菜单</h1>
+        </div>
+        
+        <div id="codearea" style="background-color:#e7e7e7;height:700px;width:700px;float:left;">
+            <b>代码区</b><br/>
+        </div>
+        <div id="midcut" style="width:100px;float:left;"><br/></div>
+        <div id="htmlarea" style="background-color:#e7e7e7;height:700px;width:700px;float:left;">
+            <b>网页区</b><br/>
+        </div>
+        <footer>
+            <div id="footer">
+            I hope life will inspire you to greater success
+            </div>
+        </footer>
+    </div>
+
+</body>
+</html>
